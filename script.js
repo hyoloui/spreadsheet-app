c14ce51a9c3bc190791f9819b8fce7fa50184387
hyoloui <tmdgy9272@gmail.com>
2023-10-15 21:33:33 +0900

    feat: create Header
    - header class를 생성합니다
    - header cell을 스타일링 합니다
    - header 는 disabled 속성을 추가하여 수정을 방지합니다
    - class 생성자에 rowName과 columnName을 추가합니다
    - 클릭한 요소의 row header와 column header에 active class를 추가합니다

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ const COLS = 10;
 const spreadsheet = [];
 
 class Cell {
-  constructor(isHeader, disabled, data, row, column, active = false) {
+  constructor(
+    isHeader,
+    disabled,
+    data,
+    row,
+    column,
+    rowName,
+    columnName,
+    active = false
+  ) {
     this.isHeader = isHeader;
     this.disabled = disabled;
     this.data = data;
     this.row = row;
     this.column = column;
+    this.rowName = rowName;
+    this.columnName = columnName;
     this.active = active;
   }
 }
@@ -20,23 +31,41 @@ function initSpreadsheet() {
   for (let i = 0; i < ROWS; i += 1) {
     const row = [];
     for (let j = 0; j < COLS; j += 1) {
+      let columnCode = String.fromCharCode(65 + j - 1);
       let cellData = "";
+      let isHeader = false;
+      let disabled = false;
 
       // 모든 셀의 첫번째 열에는 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
       if (j === 0) {
         cellData = i;
+        isHeader = true;
+        disabled = true;
       }
       // 모든 셀의 첫번째 행에는 A, B, C, D, E, F, G, H, I, J
       if (i === 0) {
-        cellData = String.fromCharCode(65 + j - 1);
+        cellData = columnCode;
+        isHeader = true;
+        disabled = true;
       }
+
+      const rowName = i;
+      let columnName = columnCode;
 
       // 0-0 에는 빈 문자열
-      if (i === 0 && j === 0) {
-        cellData = "";
-      }
+      if (i === 0 && j === 0) cellData = "";
+      if (j === 0) columnName = 0;
 
-      const cell = new Cell(false, false, cellData, i, j, false);
+      const cell = new Cell(
+        isHeader,
+        disabled,
+        cellData,
+        i,
+        j,
+        rowName,
+        columnName,
+        false
+      );
       row.push(cell); // 0-0, 0-1, 0-2, ...
     }
     spreadsheet.push(row);
@@ -51,7 +80,28 @@ function createCellElement(cell) {
   cellEl.id = "cell_" + cell.row + cell.column;
   cellEl.value = cell.data;
   cellEl.disabled = cell.disabled;
+
+  if (cell.isHeader) {
+    cellEl.classList.add("header");
+  }
+
+  cellEl.onclick = () => handleCellClick(cell);
+
   return cellEl;
+}
+
+function handleCellClick(cell) {
+  const columnHeader = spreadsheet[0][cell.column];
+  const rowHeader = spreadsheet[cell.row][0];
+  const columnHeaderEl = getElFromRowCol(columnHeader.row, columnHeader.column);
+  const rowHeaderEl = getElFromRowCol(rowHeader.row, rowHeader.column);
+  columnHeaderEl.classList.add("active");
+  rowHeaderEl.classList.add("active");
+  console.log(columnHeaderEl, rowHeaderEl);
+}
+
+function getElFromRowCol(row, col) {
+  return document.querySelector(`#cell_${row}${col}`);
 }
 
 function drawSheet() {
